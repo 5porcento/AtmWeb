70a603a256229ebb139f55c358c960c4994ff1fd
Kauan Pacheco <kauanpachequinho122@gmail.com>
2025-05-09 23:18:05 -0300

    feat: add Services to project

---
+++ src/main/java/com/example/AtmWeb/service/UserService.java
@@ -0,0 +1,15 @@
+package com.example.AtmWeb.service;
+
+import com.example.AtmWeb.repository.UserRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+    private UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+}
